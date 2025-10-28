a07791877dae1f985a8906f8fcee15579b048697
damon938 <damonmr62@gmail.com>
2025-10-28 22:39:45 +0000

    script.js

--- script.js
+++ script.js
@@ -1,51 +1,32 @@
+let players = [];
 
-const players = [
-  {
-    id: "ARK001",
-    nom: "Arken Dravos",
-    race: "Démon",
-    classe: "Guerrier",
-    pv: 20,
-    force: 18,
-    vitesse: 12,
-    magie: 6,
-    endurance: 15,
-    intelligence: 10,
-    argent: 523,
-    Victoire : 0,
-    défaite: 0,
-    inventaire: ["Épée des Ténèbres x1", "Potion de soin x5"]
-  },
-  {
-    id: "ARK002",
-    nom: "Lyra Moon",
-    race: "Vampire",
-    classe: "Nécromancien",
-    pv: 100,
-    force: 12,
-    vitesse: 14,
-    magie: 20,
-    endurance: 12,
-    intelligence: 18,
-    argent: 1200,
-    inventaire: ["Grimoire interdit x1", "Cape de nuit x1"]
-  }
-];
+window.onload = function() {
+  Tabletop.init({
+    key: "https://docs.google.com/spreadsheets/d/13WGIqcbx2NvkrEQ2m0ceYFaTkEiuHjYvIbK0YWDx53E/edit?usp=drivesdk", // Remplace par ton ID Google Sheet
+    simpleSheet: true,
+    callback: function(data, tabletop) {
+      players = data;
+      console.log("https://docs.google.com/spreadsheets/d/13WGIqcbx2NvkrEQ2m0ceYFaTkEiuHjYvIbK0YWDx53E/edit?usp=drivesdk:", players);
+    }
+  });
+};
 
 function getPlayerData() {
   const playerId = document.getElementById('playerId').value.trim();
-  const player = players.find(p => p.id === playerId);
+  const player = players.find(p => p.ID === ark001);
 
   if(player) {
     document.getElementById('carteJoueur').innerHTML = `
-      <h2>${player.nom}</h2>
-      <p><strong>Race :</strong> ${player.race}</p>
-      <p><strong>Classe :</strong> ${player.classe}</p>
-      <p><strong>PV :</strong> ${player.pv}</p>
-      <p><strong>Force :</strong> ${player.force} | <strong>Vitesse :</strong> ${player.vitesse} | <strong>Magie :</strong> ${player.magie}</p>
-      <p><strong>Endurance :</strong> ${player.endurance} | <strong>Intelligence :</strong> ${player.intelligence}</p>
-      <p><strong>Argent :</strong> ${player.argent}</p>
-      <p><strong>Inventaire :</strong> ${player.inventaire.join(", ")}</p>
+      <h2>${player.Nom}</h2>
+      <p><strong>Race :</strong> ${player.Race}</p>
+      <p><strong>Classe :</strong> ${player.Classe}</p>
+      <p><strong>PV :</strong> ${player.PV}</p>
+      <p><strong>Force :</strong> ${player.Force} | <strong>Vitesse :</strong> ${player.Vitesse} | <strong>Magie :</strong> ${player.Magie}</p>
+      <p><strong>Endurance :</strong> ${player.Endurance} | <strong>Intelligence :</strong> ${player.Intelligence}</p>
+      <p><strong>Argent :</strong> ${player.Argent}</p>
+      <p><strong>Inventaire :</strong> ${player.Inventaire}</p>
+      <p><strong>Victoires :</strong> ${player.Victoires}</p>
+      <p><strong>Défaites :</strong> ${player.Defaites}</p>
     `;
   } else {
     document.getElementById('carteJoueur').innerHTML = "ID invalide !";
